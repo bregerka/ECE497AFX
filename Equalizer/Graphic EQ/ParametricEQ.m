7c057767bb77804ff7516829e6c8fc15b4a30c29
Korey <bregerka@rose-hulman.edu>
2016-04-27 14:54:24 -0400

    PEQ
    More Updates

--- Equalizer/Graphic EQ/ParametricEQ.m
+++ Equalizer/Graphic EQ/ParametricEQ.m
@@ -15,23 +15,40 @@ values = [0 20 50 0 20 50 0 20 50 0 20 50];
  f = figure;
  b = uicontrol('Parent',f,'Style','slider','Position',[80,50,25,200],...
               'value',values(1), 'min',-30, 'max',8);
- txt = uicontrol('Style','text','Position',[50 50 25 200],'String',b.Value);
+ uicontrol('Style','text','Position',[70 300 60 50],'String','Gain(dB):');
+ txt = uicontrol('Style','text','Position',[130 300 40 50],'String',b.Value);
  b2 = uicontrol('Parent',f,'Style','slider','Position',[110,50,25,200],...
               'value',values(2), 'min',20, 'max',9000);
+ uicontrol('Style','text','Position',[60 280 80 50],'String','Center Freq:');
+ txt1 = uicontrol('Style','text','Position',[130 280 40 50],'String',b2.Value);
  b3 = uicontrol('Parent',f,'Style','slider','Position',[140,50,25,200],...
                'value',values(3), 'min',50, 'max',5000);
+ uicontrol('Style','text','Position',[70 260 60 50],'String','Bandwidth:');
+ txt2 = uicontrol('Style','text','Position',[130 260 40 50],'String',b3.Value);
  b4 = uicontrol('Parent',f,'Style','slider','Position',[200,50,25,200],...
               'value',values(4), 'min',-30, 'max',8);
+ uicontrol('Style','text','Position',[190 300 60 50],'String','Gain(dB):');
+ txt3 = uicontrol('Style','text','Position',[240 300 40 50],'String',b4.Value);
  b5 = uicontrol('Parent',f,'Style','slider','Position',[230,50,25,200],...
               'value',values(5), 'min',20, 'max',9000);
+ uicontrol('Style','text','Position',[180 280 80 50],'String','Center Freq:');
+ txt4 = uicontrol('Style','text','Position',[250 280 40 50],'String',b5.Value);
  b6 = uicontrol('Parent',f,'Style','slider','Position',[260,50,25,200],...
                'value',values(6), 'min',50, 'max',5000);
+ uicontrol('Style','text','Position',[190 260 60 50],'String','Bandwidth:');
+ txt5 = uicontrol('Style','text','Position',[250 260 40 50],'String',b6.Value);
  b7 = uicontrol('Parent',f,'Style','slider','Position',[320,50,25,200],...
               'value',values(7), 'min',-30, 'max',8);
+ uicontrol('Style','text','Position',[310 300 60 50],'String','Gain(dB):');
+ txt6 = uicontrol('Style','text','Position',[370 300 40 50],'String',b7.Value);
  b8 = uicontrol('Parent',f,'Style','slider','Position',[350,50,25,200],...
               'value',values(8), 'min',20, 'max',9000);
+ uicontrol('Style','text','Position',[300 280 80 50],'String','Center Freq:');
+ txt7 = uicontrol('Style','text','Position',[370 280 40 50],'String',b8.Value);
  b9 = uicontrol('Parent',f,'Style','slider','Position',[380,50,25,200],...
                'value',values(9), 'min',50, 'max',5000);
+ uicontrol('Style','text','Position',[310 260 60 50],'String','Bandwidth:');
+ txt8 = uicontrol('Style','text','Position',[370 260 40 50],'String',b9.Value);
  b10 = uicontrol('Parent',f,'Style','slider','Position',[440,50,25,200],...
               'value',values(7), 'min',-30, 'max',8);
  b11 = uicontrol('Parent',f,'Style','slider','Position',[470,50,25,200],...
@@ -40,7 +57,7 @@ values = [0 20 50 0 20 50 0 20 50 0 20 50];
                'value',values(9), 'min',50, 'max',5000);
 
  btn = uicontrol('Style', 'pushbutton', 'String', 'Zeroize',...
-        'Position', [300 300 50 50],...
+        'Position', [300 380 50 50],...
         'Callback','cla');  
      H = dsp.ParametricEQFilter('CenterFrequency',b2.Value,'Bandwidth',b3.Value,'PeakGaindB',b.Value);
      H2 = dsp.ParametricEQFilter('CenterFrequency',b5.Value,'Bandwidth',b6.Value,'PeakGaindB',b4.Value);
@@ -64,7 +81,15 @@ while ~isDone(audio_reader)
     H4.CenterFrequency = b11.Value;
     H4.Bandwidth = b12.Value;
     H4.PeakGaindB = b10.Value;
-    txt.String = b.Value;
+    txt.String = round(b.Value,2);
+    txt1.String = round(b2.Value);
+    txt2.String = round(b3.Value);
+    txt3.String = round(b4.Value,2);
+    txt4.String = round(b5.Value);
+    txt5.String = round(b6.Value);
+    txt6.String = round(b7.Value,2);
+    txt7.String = round(b8.Value);
+    txt8.String = round(b9.Value);
     % Retrieve the next audio frame from the file
     x = step(audio_reader);
     
