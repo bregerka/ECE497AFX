ad17d7f443438878eeea8d8de2684b401d573f27
Korey <bregerka@rose-hulman.edu>
2016-05-11 00:41:48 -0400

    basic Schroeders kind of working
    adsf

--- Reverb/SchroedersReverb.m
+++ Reverb/SchroedersReverb.m
@@ -14,7 +14,7 @@ clear, close all
 file_name = 'guitar.wav';%'snare.wav';%'A_eng_f1.wav'; % mono
 %file_name = 'Mixing Audio (Roey Izhaki)\11-014 Guitar Src.wav'; % stereo
 %audio_folder = 'C:\doering\Class\ECE497afx\resources\sounds';
-
+more_time_sec = 1;
 %% Create the audio reader and player objects
 audio_reader = dsp.AudioFileReader(file_name);
 audio_player = dsp.AudioPlayer('SampleRate', audio_reader.SampleRate);
@@ -22,20 +22,54 @@ audio_player.QueueDuration = 0; % useful for very short audio clips
 
 %% Convert the user interface values:
 %G = 2^(g_dB/6);
-delay_ms1 = 29.7;
-delay_ms2 = 37.1;
-delay_ms3 = 41.1;
-delay_ms4 = 43.7;
-G = 60;
+RT60 = 2;
+
+% Tau1 = 29.7/1000;
+% Tau2 = 37.1/1000;
+% Tau3 = 41.1/1000;
+% Tau4 = 43.7/1000;
+Tau1 = 19.7/1000;
+Tau2 = 17.1/1000;
+Tau3 = 11.1/1000;
+Tau4 = 13.7/1000;
+Tau5 = 5/1000;
+Tau6 = 1.7/1000;
 %% Convert the user interface values:
 % delay in samples and linear gain
-delay1 = (delay_ms1/1000)*audio_reader.SampleRate;
-delay2 = (delay_ms2/1000)*audio_reader.SampleRate;
-delay3 = (delay_ms3/1000)*audio_reader.SampleRate;
-delay4 = (delay_ms4/1000)*audio_reader.SampleRate;
-g = 2^(g_dB/6);
-gfb = 2^(0/6);
-
+delay1 = (Tau1)*audio_reader.SampleRate;
+delay2 = (Tau2)*audio_reader.SampleRate;
+delay3 = (Tau3)*audio_reader.SampleRate;
+delay4 = (Tau4)*audio_reader.SampleRate;
+delayAP1 = (Tau5)*audio_reader.SampleRate;
+delayAP2 = (Tau6)*audio_reader.SampleRate;
+%g = 2^(g_dB/6);
+g1 = 10^(-3*Tau1/RT60);
+g2 = 10^(-3*Tau2/RT60);
+g3 = 10^(-3*Tau3/RT60);
+g4 = 10^(-3*Tau4/RT60);
+g5 = 10^(-3*Tau5/RT60);
+g6 = 10^(-3*Tau6/RT60);
+%Number of samples to delay
+N1 = round(delay1);
+N2 = round(delay2);
+N3 = round(delay3);
+N4 = round(delay4);
+N5 = round(delayAP1);
+N6 = round(delayAP2);
+%Create Filter Coefficients for Comb Filters
+A1 = [1 zeros(1,N1-1) -g1];
+B1 = [0 zeros(1,N1-1) 1];
+A2 = [1 zeros(1,N2-1) -g2];
+B2 = [0 zeros(1,N2-1) 1];
+A3 = [1 zeros(1,N3-1) -g3];
+B3 = [0 zeros(1,N3-1) 1];
+A4 = [1 zeros(1,N4-1) -g4];
+B4 = [0 zeros(1,N4-1) 1];
+%Create Filter Coefficients for All Pass Filters
+AP1 = [1 zeros(1,N5-1) -g5];
+BP1 = [-g5 zeros(1,N5-1) 1];
+AP2 = [1 zeros(1,N6-1) -g6];
+BP2 = [-g6 zeros(1,N6-1) 1];
 %% Create the delay line object
 audio_delayline1 = dsp.Delay(round(delay1));
 audio_delayline2 = dsp.Delay(round(delay2));
@@ -53,9 +87,15 @@ while ~isDone(audio_reader)
     
     if pass_first_time
         pass_first_time = 0;
-        delayline_out = step(audio_delayline, x);
+        %y = step(audio_delayline1, x);
+        y = filter(B1,A1,x)+filter(B2,A2,x)+filter(B3,A3,x)+filter(B4,A4,x);
+        y = filter(BP1,AP1,y);
+        y = filter(BP2,AP2,y);
     else
-        delayline_out = gfb*step(audio_delayline, x);
+        %delayline_out = gfb*step(audio_delayline, x);
+        y = filter(B1,A1,x)+filter(B2,A2,x)+filter(B3,A3,x)+filter(B4,A4,x);
+        y = filter(BP1,AP1,y);
+        y = filter(BP2,AP2,y);
     end
     % Listen to the results
     step(audio_player, y);
